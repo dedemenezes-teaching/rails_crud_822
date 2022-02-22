61c5ae6befa3f76bb223dddf3cb7d22fc77d074c
Andre Menezes <djdedemenezes@gmail.com>
2022-02-22 11:14:21 -0300

    restaurant CRUD application done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'restaurants', to: 'restaurants#index', as: :restaurants
+
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # EDIT RESTAURANT
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
+
 end
